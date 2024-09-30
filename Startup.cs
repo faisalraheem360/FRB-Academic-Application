7e2c5f794b6dc7a47e94f2e13d232b8b792dbe27
Faisal Raheem Baloch <111741362+faisalraheem360@users.noreply.github.com>
2024-09-30 19:34:07 +0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-﻿using FaisalRaheemAcademicDataApp.Models;
+﻿using FRBAcademicDataApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
